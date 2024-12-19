24aa25ce9ab27372fb2736464c2d425c86821c54
kamelbataineh <batainehkamel2@gmail.com>
2024-12-19 18:34:55 +0300

    Update Screen_cities_user.dart
    aaa

--- lib/Screen_cities_user.dart
+++ lib/Screen_cities_user.dart
@@ -1,9 +1,9 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:med_project_flutter2/Screen_cities_rented.dart';
 import 'package:med_project_flutter2/Screen_favorite.dart';
 import 'package:med_project_flutter2/Screen_setting.dart';
 import 'package:med_project_flutter2/consr_routes.dart';
+import 'package:med_project_flutter2/userORrented.dart';
 
 class ScreenCitiesUser extends StatefulWidget {
   @override
@@ -14,69 +14,67 @@ class ScreenCitiesUser extends StatefulWidget {
 
 class _HomeState extends State<ScreenCitiesUser> {
   List<Widget> pages = [
-ScreenCitiesUser(),
-    ScreenSetting(),
-    ScreenFavorite(),
-    ScreenFavorite(),
 
+    ScreenSetting(),
+    ScreenFavorite()
   ];
-  int indexpage = 0;
+  int indexpage =0;
 
   final List<Map<String, String>> cities = [
     {
       'name': 'اربد',
       'image':
-          'https://i.pinimg.com/564x/e0/5a/34/e05a3477a58ac454e827aac2ab2ec03c.jpg',
+      'https://i.pinimg.com/564x/e0/5a/34/e05a3477a58ac454e827aac2ab2ec03c.jpg',
     },
     {
       'name': 'جرش',
       'image':
-          'https://i.pinimg.com/474x/00/e1/f4/00e1f45e889bb474e710a4533c50d544.jpg',
+      'https://i.pinimg.com/474x/00/e1/f4/00e1f45e889bb474e710a4533c50d544.jpg',
     },
     {
       'name': 'عجلون',
       'image':
-          'https://i.pinimg.com/564x/d4/da/1f/d4da1f8c3742f9501681a9e607a29920.jpg',
+      'https://i.pinimg.com/564x/d4/da/1f/d4da1f8c3742f9501681a9e607a29920.jpg',
     },
     {
       'name': 'المفرق',
       'image':
-          'https://i.pinimg.com/736x/8d/41/ea/8d41ea639119ca1dc2628b76fde9ca8b.jpg',
+      'https://i.pinimg.com/736x/8d/41/ea/8d41ea639119ca1dc2628b76fde9ca8b.jpg',
     },
     {
       'name': 'عمان',
       'image':
-          'https://i.pinimg.com/564x/03/b4/ff/03b4ffbe9860921a696898d41fe25091.jpg',
+      'https://i.pinimg.com/564x/03/b4/ff/03b4ffbe9860921a696898d41fe25091.jpg',
     },
     {
       'name': 'مادبا',
       'image':
-          'https://i.pinimg.com/564x/d2/f6/28/d2f628a0612545a60f4781974674149a.jpg',
+      'https://i.pinimg.com/564x/d2/f6/28/d2f628a0612545a60f4781974674149a.jpg',
     },
     {
       'name': 'البلقاء',
       'image':
-          'https://i.pinimg.com/236x/71/70/69/717069eb2e3f4d752808749870c1464f.jpg',
+      'https://i.pinimg.com/236x/71/70/69/717069eb2e3f4d752808749870c1464f.jpg',
     },
     {
       'name': 'الكرك',
       'image':
-          'https://i.pinimg.com/564x/13/58/cf/1358cf4a6077d0f67d13ece19f148eb7.jpg',
+      'https://i.pinimg.com/564x/13/58/cf/1358cf4a6077d0f67d13ece19f148eb7.jpg',
     },
     {
       'name': 'الطفيلة',
       'image':
-          'https://i.pinimg.com/736x/48/13/49/4813497936c9154ff6060fb2ef5c0db8.jpg',
+      'https://i.pinimg.com/736x/48/13/49/4813497936c9154ff6060fb2ef5c0db8.jpg',
     },
     {
       'name': 'معان',
       'image':
-          'https://i.pinimg.com/564x/b9/e4/25/b9e425b64550bb7e3b45a272d7de6f74.jpg',
+      'https://i.pinimg.com/564x/b9/e4/25/b9e425b64550bb7e3b45a272d7de6f74.jpg',
     },
     {
       'name': 'العقبة',
       'image':
-          'https://i.pinimg.com/564x/9f/d6/83/9fd683479f339e1f9d2a6a62958c379a.jpg',
+      'https://i.pinimg.com/564x/9f/d6/83/9fd683479f339e1f9d2a6a62958c379a.jpg',
     },
   ];
   List<String> list = ["تسجيل دخول", "حساب جديد", "تسجيل خروج"];
@@ -86,76 +84,78 @@ ScreenCitiesUser(),
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Align(
-            alignment: Alignment.center,
-            child: Text("المدن"),
-          ),
-          backgroundColor: Colors.blue,
-          actions: [
-            PopupMenuButton(
-                icon: Icon(
-                  Icons.density_medium,
-                  size: 25,
-                  color: Colors.black,
-                ),
-                itemBuilder: (context) {
-                  return list.map((element) {
-                    return PopupMenuItem(
-                      value: element,
-                      child: Row(
-                        children: [
-                          Icon(
-                            element == "تسجيل دخول"
-                                ? Icons.person
-                                : element == "حساب جديد"
-                                    ? Icons.person_add
-                                    : Icons.logout,
-                            color: Colors.black54,
-                            size: 25,
-                          ),
-                          SizedBox(width: 30),
-                          Text(
-                            element,
-                            style:
-                                TextStyle(fontSize: 20, color: Colors.black87),
-                          ),
-                        ],
-                      ),
-                    );
-                  }).toList();
-                },
-                color: Colors.white,
-                onCanceled: () {
-                  print("object");
-                },
-                onSelected: (value) {
-                  setState(() {
-                    select = value;
-                    switch (select) {
-                      case "تسجيل دخول":
-                        Navigator.of(context).pushNamed(route_Userorrented);
-
-                        break;
-                      case "حساب جديد":
-                        Navigator.of(context)
-                            .pushNamed(route_ScreenRegisterUser);
-
-                        break;
-                      case "تسجيل خروج":
-                        Navigator.of(context).pushNamed(route_Userorrented);
-                        break;
-                      default:
-                        Navigator.of(context).pushNamed(route_ScreenCitiesUser);
-                    }
-                  });
-                })
-          ],
+      appBar: AppBar(
+        title: Align(
+          alignment: Alignment.center,
+          child: Text("المدن"),
         ),
+        backgroundColor: Colors.blue,
+        actions: [
+          PopupMenuButton(
+              icon: Icon(
+                Icons.density_medium,
+                size: 25,
+                color: Colors.black,
+              ),
+              itemBuilder: (context) {
+                return list.map((element) {
+                  return PopupMenuItem(
+                    value: element,
+                    child: Row(
+                      children: [
+                        Icon(
+                          element == "تسجيل دخول"
+                              ? Icons.person
+                              : element == "حساب جديد"
+                              ? Icons.person_add
+                              : Icons.logout,
+                          color: Colors.black54,
+                          size: 25,
+                        ),
+                        SizedBox(width: 30),
+                        Text(
+                          element,
+                          style:
+                          TextStyle(fontSize: 20, color: Colors.black87),
+                        ),
+                      ],
+                    ),
+                  );
+                }).toList();
+              },
+              color: Colors.white,
+              onCanceled: () {
+                print("object");
+              },
+              onSelected: (value) {
+                setState(() {
+                  select = value;
+                  switch (select) {
+                    case "تسجيل دخول":
+                      Navigator.of(context).pushNamed(route_Userorrented);
+
+                      break;
+                    case "حساب جديد":
+                      Navigator.of(context)
+                          .pushNamed(route_ScreenRegisterUser);
+
+                      break;
+                    case "تسجيل خروج":
+                      Navigator.of(context).pushNamed(route_Userorrented);
+                      break;
+                    default:
+                      Navigator.of(context).pushNamed(route_ScreenCitiesUser);
+                  }
+                });
+              })
+        ],
+      ),
       body: Stack(
+
         children: [
 
-           GridView.builder(
+          Expanded(
+            child: GridView.builder(
               gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
                 crossAxisSpacing: 8.0,
@@ -167,14 +167,12 @@ ScreenCitiesUser(),
               itemBuilder: (context, index) {
                 return City(index);
               },
-
+            ),
           ),
-IndexedStack(
-
-  index: indexpage,
-  children:pages,
-)
-        ],
+          IndexedStack(
+            index: indexpage,
+            children: pages,
+          ), ],
       ),
 
       bottomNavigationBar: CurvedNavigationBar(
@@ -182,10 +180,10 @@ IndexedStack(
         index: indexpage,
         onTap: (index) {
           setState(() {
-            indexpage = index; 
+            indexpage = index;
           });
         },
-        items: const [
+        items: [
           Icon(Icons.home),
           Icon(Icons.search),
           Icon(Icons.notifications),
@@ -195,7 +193,8 @@ IndexedStack(
         backgroundColor: Colors.white,
         buttonBackgroundColor: Colors.black,
         animationDuration: Duration(milliseconds: 300),
-      ),
+      )
+      ,
     );
   }
 
@@ -267,38 +266,38 @@ IndexedStack(
                   alignment: Alignment.center,
                 ),
               ),
-      Card(
-        color: Colors.black54,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15.0),
-        ),
-        elevation: 2,
-        child: Container(
-          padding: EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.black54, Colors.transparent],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-            borderRadius: BorderRadius.circular(15.0),
-          ),
-          child: Text(
-            cities[index]['name']!,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 15,
-              fontWeight: FontWeight.bold,
-              shadows: [
-                Shadow(
-                  blurRadius: 5.0,
-                  color: Colors.black.withOpacity(0.7),
+              Card(
+                color: Colors.black54,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
                 ),
-              ],
-            ),
-          ),
-        ),
-      )
+                elevation: 2,
+                child: Container(
+                  padding: EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Colors.black54, Colors.transparent],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: Text(
+                    cities[index]['name']!,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      shadows: [
+                        Shadow(
+                          blurRadius: 5.0,
+                          color: Colors.black.withOpacity(0.7),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
 
@@ -306,5 +305,4 @@ IndexedStack(
 
     );
 
-  }
-}
+  }}
