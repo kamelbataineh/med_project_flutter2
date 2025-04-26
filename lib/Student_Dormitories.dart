9ed7850a24076f7b9dae2fc5bd697f3f4dede8a9
kamelbataineh <batainehkamel2@gmail.com>
2025-04-26 11:33:14 +0300

    4/26

--- lib/Student_Dormitories.dart
+++ lib/Student_Dormitories.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'Housing_Details_Page.dart';
 
 class StudentDormitories extends StatefulWidget {
   const StudentDormitories({super.key});
@@ -6,43 +8,65 @@ class StudentDormitories extends StatefulWidget {
   @override
   State<StudentDormitories> createState() => _StudentDormitoriesState();
 }
+
 class _StudentDormitoriesState extends State<StudentDormitories> {
   final List<String> universities = [
-    "الجامعة الأردنية",
-    "جامعة اليرموك",
-    "جامعة مؤتة",
-    "جامعة العلوم والتكنولوجيا",
-    "جامعة البلقاء التطبيقية",
-    "جامعة الحسين بن طلال",
-    "جامعة الطفيلة التقنية",
-    "جامعة آل البيت",
-    "الجامعة الألمانية الأردنية",
-    "جامعة الزيتونة الأردنية"
+    "University of Jordan",
+    "Yarmouk University",
+    "Mutah University",
+    "Jordan University of Science and Technology",
+    "Al-Balqa Applied University",
+    "Al-Hussein Bin Talal University",
+    "Tafila Technical University",
+    "Al al-Bayt University",
+    "German-Jordanian University",
+    "Zarqa Private University"
   ];
+  String? selectedGender;
+  String? selectedUniversities;
+  List<DocumentSnapshot> housings = [];
+  bool isLoading = false;
+  bool showHousing = false;
 
-  final Map<String, List<String>> dormitories = {
-    "الجامعة الأردنية": ["سكن عمان", "سكن الزهور"],
-    "جامعة اليرموك": ["سكن النخبة", "سكن الشباب"],
-    "جامعة مؤتة": ["سكن الكرامة", "سكن الأمان"],
-    "جامعة العلوم والتكنولوجيا": ["سكن المستقبل", "سكن الريادة"],
-    "جامعة البلقاء التطبيقية": ["سكن المهندسين", "سكن الطموح"],
-    "جامعة الحسين بن طلال": ["سكن الريف", "سكن الأقصى"],
-    "جامعة الطفيلة التقنية": ["سكن الإبداع", "سكن الزهور"],
-    "جامعة آل البيت": ["سكن النور", "سكن الصفوة"],
-    "الجامعة الألمانية الأردنية": ["سكن الراحة", "سكن الهدى"],
-    "جامعة الزيتونة الأردنية": ["سكن المستقبل", "سكن الحياة"],
-  };
+  final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
 
-  String? selectedUniversity;
-  String? selectedDormitory;
-  String selectedGender = "طلاب";
-  String confirmationMessage = "";
+  Future<void> getHousing() async {
+    if (selectedUniversities == null) {
+      setState(() {
+        housings = [];
+        isLoading = false;
+      });
+      return;
+    }
+
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      QuerySnapshot querySnapshot = await firebaseFirestore
+          .collection('AddHousing')
+          .where('region', isEqualTo: selectedUniversities)
+          .where('residentType', isEqualTo: selectedGender)
+          .get();
+
+      setState(() {
+        housings = querySnapshot.docs;
+      });
+    } catch (e) {
+      print("Error fetching housing data: $e");
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
+        title: Text(
           "Student Dormitories",
           style: TextStyle(fontSize: 20),
         ),
@@ -55,164 +79,160 @@ class _StudentDormitoriesState extends State<StudentDormitories> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-               Align(
-                 alignment: Alignment.centerLeft,
-                 child: Text(
-                  "Choose a university",
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                               ),
-               ),
-              Divider(
-                color: Colors.grey[400],
-                thickness: 1,
-                indent: 1,
-                endIndent: 1,
+              Text(
+                "Choose a city",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
               ),
-              SizedBox(
-                height: 10,
-              ),
+              Divider(color: Colors.grey[400], thickness: 1),
+              SizedBox(height: 10),
               DropdownButtonFormField<String>(
-                value: selectedUniversity,
-                hint: const Text("اختر الجامعة"),
-                items: universities.map((String university) {
+                value: selectedUniversities,
+                hint: Text("Choose a city"),
+                items: universities.map((String city) {
                   return DropdownMenuItem<String>(
-                    value: university,
-                    child: Text(university),
+                    value: city,
+                    child: Text(city),
                   );
                 }).toList(),
                 onChanged: (String? value) {
                   setState(() {
-                    selectedUniversity = value;
-                    selectedDormitory = null;
-                    confirmationMessage = "";
+                    selectedUniversities = value;
+                    showHousing = false;
                   });
+                  getHousing();
                 },
-                dropdownColor: Colors.white,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
                 ),
               ),
-              const SizedBox(height: 20),
-              if (selectedUniversity != null) ...[
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Available accommodation",
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Divider(
-                  color: Colors.grey[400],
-                  thickness: 1,
-                  indent: 1,
-                  endIndent: 1,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                DropdownButtonFormField<String>(
-                  dropdownColor: Colors.white,
-                  value: selectedDormitory,
-                  hint: const Text("اختر السكن"),
-                  items: dormitories[selectedUniversity]!.map((String dorm) {
-                    return DropdownMenuItem<String>(
-                      value: dorm,
-                      child: Text(dorm),
-                    );
-                  }).toList(),
-                  onChanged: (String? value) {
-                    setState(() {
-                      selectedDormitory = value;
-                      confirmationMessage = "";
-                    });
-                  },
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                  ),
-                ),
-              ],
+
               SizedBox(height: 30),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Gender",
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                ),
-              ),
-              Divider(
-                color: Colors.grey[400],
-                thickness: 1,
-                indent: 1,
-                endIndent: 1,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
+            Divider(color: Colors.grey[400], thickness: 1),
+            SizedBox(height: 10),
+            Row(
                 children: [
                   Expanded(
                     child: RadioListTile<String>(
-                      title:Text("طلاب"),
-                      value: "طلاب",
+                      title: Text("Male Students", style: TextStyle(fontSize: 15)),
+                      value: "Male Students",
                       groupValue: selectedGender,
-                      onChanged: (value) {
+                      onChanged: (String? value) {
                         setState(() {
-                          selectedGender = value!;
+                          selectedGender = value;
+                          showHousing = false;
                         });
+                        getHousing();
                       },
                     ),
                   ),
                   Expanded(
                     child: RadioListTile<String>(
-                      title:Text("طالبات"),
-                      value:"طالبات",
+                      title: Text("Female Students", style: TextStyle(fontSize: 15)),
+                      value: "Female Students",
                       groupValue: selectedGender,
-                      onChanged: (value) {
+                      onChanged: (String? value) {
                         setState(() {
-                          selectedGender = value!;
+                          selectedGender = value;
+                          showHousing = false;
                         });
+                        getHousing();
                       },
                     ),
                   ),
-                ],
-              ),
-              SizedBox(height: 40),
+                ],),
               Center(
                 child: ElevatedButton(
-                  onPressed: () {
-                    if (selectedUniversity != null && selectedDormitory != null) {
-                      setState(() {
-                        confirmationMessage =
-                        "University:$selectedUniversity\n"
-                        "Housing:$selectedDormitory\nGender: $selectedGender";
-                      });
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                         SnackBar(
-                          content: Text("يرجى اختيار الجامعة والسكن."),
-                        ),
-                      );
-                    }
+                  onPressed: housings.isEmpty
+                      ? null
+                      : () {
+                    setState(() {
+                      showHousing = !showHousing;
+                    });
                   },
+                  child: Text(showHousing ? "Hide Information" : "Show Information"),
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue[900],
-                    padding:EdgeInsets.symmetric(horizontal: 50, vertical: 5),
-                  ),
-                  child:Text(
-                    "Show",
-                    style: TextStyle(fontSize: 18,color: Colors.black),
+                    backgroundColor: Colors.green,
                   ),
                 ),
               ),
-               SizedBox(height: 20),
-              if (confirmationMessage.isNotEmpty)
-                Center(
-                  child: Text(
-                    confirmationMessage,
-                    style:TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
-                    textAlign: TextAlign.center,
+
+
+              SizedBox(height: 30),
+              if (isLoading)
+                Center(child: CircularProgressIndicator())
+              else if (showHousing && housings.isNotEmpty)
+                GridView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 8.0,
+                    mainAxisSpacing: 8.0,
+                    childAspectRatio: 0.75,
                   ),
-                ),
+                  itemCount: housings.length,
+                  itemBuilder: (context, index) {
+                    final house = housings[index].data() as Map<String, dynamic>;
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => HousingDetailsPage(house),
+                          ),
+                        );
+                      },
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        elevation: 10,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
+                                child: Image.network(
+                                  house['images']?.isNotEmpty ?? false
+                                      ? house['images'][0]
+                                      : 'https://via.placeholder.com/150',
+                                  width: double.infinity,
+                                  height: double.infinity,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    house['housingName'] ?? 'Unknown',
+                                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                                  ),
+                                  SizedBox(height: 4),
+                                  Text(
+                                    '${house['governorate'] ?? ''}\n${house['residentType'] ?? ''} \$${house['price']?['monthly'] ?? ''}',
+                                    style: TextStyle(color: Colors.grey[600], fontSize: 14),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                )
+              else if (showHousing)
+                  Center(
+                    child: Text(
+                      "No housing available for the selected city.",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
             ],
           ),
         ),
@@ -221,4 +241,60 @@ class _StudentDormitoriesState extends State<StudentDormitories> {
   }
 }
 
+
+// هاي اذا بدي اضيف خيار ثاني
+
+//
+// const SizedBox(height: 20),
+// if (selectedUniversity != null) ...[
+// Align(
+// alignment: Alignment.centerLeft,
+// child: Text(
+// "Available accommodation",
+// style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+// ),
+// ),
+// Divider(
+// color: Colors.grey[400],
+// thickness: 1,
+// indent: 1,
+// endIndent: 1,
+// ),
+// SizedBox(
+// height: 10,
+// ),
+// DropdownButtonFormField<String>(
+// dropdownColor: Colors.white,
+// value: selectedDormitory,
+// hint: const Text("Choose housing"),
+// items: dormitories[selectedUniversity]!.map((String dorm) {
+// return DropdownMenuItem<String>(
+// value: dorm,
+// child: Text(dorm),
+// );
+// }).toList(),
+// onChanged: (String? value) {
+// setState(() {
+// selectedDormitory = value;
+// confirmationMessage = "";
+// });
+// },
+// decoration: InputDecoration(
+// border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+// ),
+// ),
+// ],
+//
+// final Map<String, List<String>> dormitories = {
+//   "University of Jordan": ["Amman Dorm", "Zuhur Dorm"],
+//   "Yarmouk University": ["Elite Dorm", "Shabab Dorm"],
+//   "Mutah University": ["Karama Dorm", "Aman Dorm"],
+//   "Jordan University of Science and Technology": ["Mostaqbal Dorm", "Riyada Dorm"],
+//   "Al-Balqa Applied University": ["Engineers Dorm", "Tomooh Dorm"],
+//   "Al-Hussein Bin Talal University": ["Reef Dorm", "Aqsa Dorm"],
+//   "Tafila Technical University": ["Ebda3 Dorm", "Zuhur Dorm"],
+//   "Al al-Bayt University": ["Noor Dorm", "Sofwa Dorm"],
+//   "German-Jordanian University": ["Raha Dorm", "Huda Dorm"],
+//   "Zarqa Private University": ["Mostaqbal Dorm", "Hayah Dorm"],
+// };
 
